24f188a7b5ce633192fe2a9b6b16ef531b52774d
jasper.kelder <jasperkelder@gmail.com>
2022-08-09 19:51:15 +0200

    SSR and prefetech update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,29 @@
 <script context="module">
-	export const load = async ({ fetch }) => {
-		const res = await fetch('/api/posts.json');
-		const posts = await res.json();
+  import { BASE_URL, POSTS_API_URL } from '$lib/variables';
 
-		return {
-			props: {
-				posts
-			}
-		};
-	};
+  export const load = async () => {
+    const res = await fetch(`${BASE_URL}${POSTS_API_URL}?_embed`);
+    const posts = await res.json();
+    posts.map(post => {
+      post.image = post._embedded['wp:featuredmedia'][0].source_url;
+    });
+
+    return {
+      props: {
+        posts,
+      },
+    };
+  };
 </script>
 
 <script>
-	export let posts;
+  export let posts;
 </script>
 
-<h1>Svelte Wordpress</h1>
+<h1>SvelteKit Wordpress</h1>
 {#each posts as post}
-	<h2>{@html post.title.rendered}</h2>
-	<img src={post.image} alt={post.title.rendered} />
-	<p>{@html post.excerpt.rendered}</p>
-	<a href={`/posts/${post.slug}`}>Read More</a>
+  <h2>{@html post.title.rendered}</h2>
+  <img src={post.image} alt={post.title.rendered} />
+  <p>{@html post.excerpt.rendered}</p>
+  <a sveltekit:prefetch href={`/posts/${post.slug}`}>Read More</a>
 {/each}
--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -1,25 +1,30 @@
 <script context="module">
-	export const load = async ({ params, fetch }) => {
-		const slug = params.slug;
-		const res = await fetch(`/api/posts/${slug}.json`);
-		const post = await res.json();
+  import { BASE_URL, POSTS_API_URL } from '$lib/variables';
 
-		return {
-			props: {
-				post
-			}
-		};
-	};
+  export const load = async request => {
+    const slug = request.params.slug;
+    const res = await fetch(`${BASE_URL}${POSTS_API_URL}?slug=${slug}&_embed`);
+    const posts = await res.json();
+    const post = posts[0];
+    post.image = post._embedded['wp:featuredmedia'][0].source_url;
+    post.author = post._embedded.author[0].name;
+
+    return {
+      props: {
+        post,
+      },
+    };
+  };
 </script>
 
 <script>
-	export let post;
+  export let post;
 </script>
 
 <h1>{@html post.title.rendered}</h1>
 <p><small>{new Date(post.date)}</small></p>
 <p><b>by {post.author}</b></p>
 {#if post.image}
-	<img src={post.image} alt={post.title.rendered} />
+  <img src={post.image} alt={post.title.rendered} />
 {/if}
 <p>{@html post.content.rendered}</p>
