a431e5cb813788dc1f7aaf4be9c6d7b34fa69c9c
jasper.kelder <jasperkelder@gmail.com>
2022-08-09 19:28:31 +0200

    Initial commit

---
+++ src/routes/posts/[slug].svelte
@@ -0,0 +1,25 @@
+<script context="module">
+	export const load = async ({ params, fetch }) => {
+		const slug = params.slug;
+		const res = await fetch(`/api/posts/${slug}.json`);
+		const post = await res.json();
+
+		return {
+			props: {
+				post
+			}
+		};
+	};
+</script>
+
+<script>
+	export let post;
+</script>
+
+<h1>{@html post.title.rendered}</h1>
+<p><small>{new Date(post.date)}</small></p>
+<p><b>by {post.author}</b></p>
+{#if post.image}
+	<img src={post.image} alt={post.title.rendered} />
+{/if}
+<p>{@html post.content.rendered}</p>
